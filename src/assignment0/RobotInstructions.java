409b450c067c99edbb1ed22e8e78a7f8b7f8c987
SeanBGreen <green.s.b@wustl.edu>
2025-01-20 20:24:24 -0600

    Update RobotInstructions.java

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,50 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(150);
+		robot.turnLeft(45);
+		robot.forward(300);
+		robot.turnRight(45);
+		robot.forward(75);
+		robot.turnLeft(90);
+		robot.forward(75);
+		robot.turnLeft(90);
+		robot.forward(100);
+		
+		robot.turnRight(90);
+		robot.forward(100);
+		robot.turnRight(180);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(20);
+		
+		robot.turnLeft(45);
+		robot.forward(230);
+		robot.turnLeft(45);
+		robot.forward(125);
+		robot.turnRight(90);
+		robot.forward(500);
+		robot.turnRight(90);
+		robot.forward(500);
+		robot.turnRight(90);
+		robot.forward(495);
+		robot.turnRight(90);
+		robot.forward(400);
+		robot.turnRight(90);
+		robot.forward(400);
+		robot.turnRight(90);
+		robot.forward(300);
+		robot.turnRight(90);
+		robot.forward(300);
+		robot.turnRight(90);
+		robot.forward(200);
+		robot.turnRight(90);
+		robot.forward(200);
+		robot.turnRight(90);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(100);
 	}
 
 }
